44112b6444e670c969400a7cc5a98bcf607e290d
Dragon <paramate.2547@gmail.com>
2022-09-10 13:17:44 +0700

    Fix bug

--- 1_1_even.c
+++ 1_1_even.c
@@ -2,20 +2,22 @@
 
 int main()
 {
-
     int x, y, z;
-
     printf("Enter three numbers: ");
     scanf("%d %d %d", &x, &y, &z);
-    if (x >= y && x >= z)
+    if (x == y && x == z)
     {
         printf("%d is the maximum number.", x);
     }
-    if (y >= x && y >= z)
+    else if (x >= y && x >= z)
+    {
+        printf("%d is the maximum number.", x);
+    }
+    else if (y >= x && y >= z)
     {
         printf("%d is the maximum number.", y);
     }
-    if (z >= x && z >= y)
+    else if (z >= x && z >= y)
     {
         printf("%d is the maximum number.", z);
     }
